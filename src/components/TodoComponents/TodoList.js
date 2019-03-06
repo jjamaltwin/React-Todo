7de21228efad5b72cc9508812f25b29b2be93212
unknown <jjamal95@gmail.com>
2019-03-05 20:48:48 -0600

    Initoal push, added input form , and add and clear completed buttons

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,38 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React, { Component } from 'react';
+
+
+// created class component with constructor with props to pass attributes
+
+class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+
+
+        // set the state equal to an array
+        this.state = {
+            items: []
+        };
+
+    }
+   
+
+    render() {
+     
+
+        return (
+            <div className="todoListMain">
+                <div className='header'>
+                    <form >
+                        <input 
+                            placeholder="Enter ToDo item">
+                        </input>
+                        <button type="submit" >Add ToDo</button>
+                        <button>Clear Completed</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
