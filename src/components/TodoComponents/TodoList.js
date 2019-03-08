a547ba6c5f5251accaf03db71c4b7cc47c536b85
unknown <jjamal95@gmail.com>
2019-03-07 21:28:38 -0600

    working on search functionality , and adding pomodoro image and timer

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './Todo.css';
 import Todo from './Todo'
 
-// created class component with constructor with props to pass attributes
+// created class component with constructor and props to pass attributes
+ 
 
 class ToDoList extends Component {
     constructor(props) {
@@ -66,6 +67,97 @@ class ToDoList extends Component {
             items: filteredItems
         });
     }
+
+     autocomplete = (inp, items) => {
+       
+        let currentFocus;
+        
+        inp.addEventListener("input", function(e) {
+            var a, b, i, val = this.value;
+           
+            closeAllLists();
+            if (!val) { return false;}
+            currentFocus = -1;
+           
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+           
+            this.parentNode.appendChild(a);
+           
+            for (i = 0; i < items.length; i++) {
+              
+              if (items[i].substr(0, val.length).toUpperCase() === val.toUpperCase()) {
+                
+                b = document.createElement("DIV");
+             
+                b.innerHTML = "<strong>" + items[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += items[i].substr(val.length);
+               
+                b.innerHTML += "<input type='hidden' value='" + items[i] + "'>";
+                
+                    b.addEventListener("click", function(e) {
+                 
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    
+                    closeAllLists();
+                });
+                a.appendChild(b);
+              }
+            }
+        });
+        
+        inp.addEventListener("keydown", function(e) {
+            var x = document.getElementById(this.id + "autocomplete-list");
+            if (x) x = x.getElementsByTagName("div");
+            if (e.keyCode === 40) {
+           
+              currentFocus++;
+              
+              addActive(x);
+            } else if (e.keyCode === 38) { 
+              currentFocus--;
+              
+              addActive(x);
+            } else if (e.keyCode === 13) {
+              
+              e.preventDefault();
+              if (currentFocus > -1) {
+               
+                if (x) x[currentFocus].click();
+              }
+            }
+        });
+        function addActive(x) {
+          
+          if (!x) return false;
+          
+          removeActive(x);
+          if (currentFocus >= x.length) currentFocus = 0;
+          if (currentFocus < 0) currentFocus = (x.length - 1);
+          
+          x[currentFocus].classList.add("autocomplete-active");
+        }
+        function removeActive(x) {
+          
+          for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+          }
+        }
+        function closeAllLists(elmnt) {
+        
+          var x = document.getElementsByClassName("autocomplete-items");
+          for (var i = 0; i < x.length; i++) {
+            if (elmnt !== x[i] && elmnt !== inp) {
+            x[i].parentNode.removeChild(x[i]);
+          }
+        }
+      }
+      
+      document.addEventListener("click", function (e) {
+          closeAllLists(e.target);
+      });
+      }
    
 
     render() {
@@ -74,12 +166,19 @@ class ToDoList extends Component {
         return (
             <div className="todoListMain">
                 <div className='header'>
-                    <form onSubmit={this.addItem} >
+                    <form autocomplete="off" action="/action_page.php" onSubmit={this.addItem} >
                         <input ref={(a) => this._inputElement = a}
                             placeholder="Enter ToDo item">
                         </input>
                         <button type="submit" >Add ToDo</button>
                         <button onClick={this.deleteItem}>Clear Completed</button>
+                    <img src="http://cdn.shopify.com/s/files/1/1940/0783/products/Tomato_timer_01_1024x1024.png?v=1523492026" alt="tomato-pic"></img>
+                        <div className="autocomplete" >
+                        <input id="myInput" type="text" name="myTodo" placeholder="Search Todo">
+                        </input >
+                        
+                        </div>
+
                     </form>
                 </div>
                 <Todo entries={this.state.items}
