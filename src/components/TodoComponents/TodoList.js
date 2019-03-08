32a23e15db31dd95179f924fa3de5629bcf8e3af
unknown <jjamal95@gmail.com>
2019-03-07 22:13:58 -0600

    added pomodoro timer and image, still trying to figure out, auto complete

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,7 +3,7 @@ import './Todo.css';
 import Todo from './Todo'
 
 // created class component with constructor and props to pass attributes
- 
+
 
 class ToDoList extends Component {
     constructor(props) {
@@ -13,6 +13,92 @@ class ToDoList extends Component {
         // set the state equal to an array
         this.state = {
             items: []
+        };
+        //  Pomodoro functionality 
+        var pomodoro = {
+            started: false,
+            minutes: 0,
+            seconds: 0,
+            fillerHeight: 0,
+            fillerIncrement: 0,
+            interval: null,
+            minutesDom: null,
+            secondsDom: null,
+            fillerDom: null,
+            init: function () {
+                var self = this;
+                this.minutesDom = document.querySelector('#minutes');
+                this.secondsDom = document.querySelector('#seconds');
+                this.fillerDom = document.querySelector('#filler');
+                this.interval = setInterval(function () {
+                    self.intervalCallback.apply(self);
+                }, 1000);
+                document.querySelector('#work').onclick = function () {
+                    self.startWork.apply(self);
+                };
+                document.querySelector('#shortBreak').onclick = function () {
+                    self.startShortBreak.apply(self);
+                };
+                document.querySelector('#longBreak').onclick = function () {
+                    self.startLongBreak.apply(self);
+                };
+                document.querySelector('#stop').onclick = function () {
+                    self.stopTimer.apply(self);
+                };
+            },
+            resetVariables: function (mins, secs, started) {
+                this.minutes = mins;
+                this.seconds = secs;
+                this.started = started;
+                this.fillerIncrement = 200 / (this.minutes * 60);
+                this.fillerHeight = 0;
+            },
+            startWork: function () {
+                this.resetVariables(25, 0, true);
+            },
+            startShortBreak: function () {
+                this.resetVariables(5, 0, true);
+            },
+            startLongBreak: function () {
+                this.resetVariables(15, 0, true);
+            },
+            stopTimer: function () {
+                this.resetVariables(25, 0, false);
+                this.updateDom();
+            },
+            toDoubleDigit: function (num) {
+                if (num < 10) {
+                    return "0" + parseInt(num, 10);
+                }
+                return num;
+            },
+            updateDom: function () {
+                this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+                this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+                this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+                this.fillerDom.style.height = this.fillerHeight + 'px';
+            },
+            intervalCallback: function () {
+                if (!this.started) return false;
+                if (this.seconds == 0) {
+                    if (this.minutes == 0) {
+                        this.timerComplete();
+                        return;
+                    }
+                    this.seconds = 59;
+                    this.minutes--;
+                } else {
+                    this.seconds--;
+                }
+                this.updateDom();
+            },
+            timerComplete: function () {
+                this.started = false;
+                this.fillerHeight = 0;
+            }
+        };
+        window.onload = function () {
+            pomodoro.init();
         };
 
     }
@@ -48,7 +134,7 @@ class ToDoList extends Component {
             this.setState((prevState) => {
                 return {
                     // combining the two arrays
-                    items: prevState.items.concat(newItem)  
+                    items: prevState.items.concat(newItem)
                 };
             });
         }
@@ -68,125 +154,149 @@ class ToDoList extends Component {
         });
     }
 
-     autocomplete = (inp, items) => {
-       
+    autocomplete = (inp, items) => {
+
         let currentFocus;
-        
-        inp.addEventListener("input", function(e) {
+
+        inp.addEventListener("input", function (e) {
             var a, b, i, val = this.value;
-           
+
             closeAllLists();
-            if (!val) { return false;}
+            if (!val) { return false; }
             currentFocus = -1;
-           
+
             a = document.createElement("DIV");
             a.setAttribute("id", this.id + "autocomplete-list");
             a.setAttribute("class", "autocomplete-items");
-           
+
             this.parentNode.appendChild(a);
-           
+
             for (i = 0; i < items.length; i++) {
-              
-              if (items[i].substr(0, val.length).toUpperCase() === val.toUpperCase()) {
-                
-                b = document.createElement("DIV");
-             
-                b.innerHTML = "<strong>" + items[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += items[i].substr(val.length);
-               
-                b.innerHTML += "<input type='hidden' value='" + items[i] + "'>";
-                
-                    b.addEventListener("click", function(e) {
-                 
-                    inp.value = this.getElementsByTagName("input")[0].value;
-                    
-                    closeAllLists();
-                });
-                a.appendChild(b);
-              }
+
+                if (items[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
+                    b = document.createElement("DIV");
+
+                    b.innerHTML = "<strong>" + items[i].substr(0, val.length) + "</strong>";
+                    b.innerHTML += items[i].substr(val.length);
+
+                    b.innerHTML += "<input type='hidden' value='" + items[i] + "'>";
+
+                    b.addEventListener("click", function (e) {
+
+                        inp.value = this.getElementsByTagName("input")[0].value;
+
+                        closeAllLists();
+                    });
+                    a.appendChild(b);
+                }
             }
         });
-        
-        inp.addEventListener("keydown", function(e) {
+
+        inp.addEventListener("keydown", function (e) {
             var x = document.getElementById(this.id + "autocomplete-list");
             if (x) x = x.getElementsByTagName("div");
-            if (e.keyCode === 40) {
-           
-              currentFocus++;
-              
-              addActive(x);
-            } else if (e.keyCode === 38) { 
-              currentFocus--;
-              
-              addActive(x);
-            } else if (e.keyCode === 13) {
-              
-              e.preventDefault();
-              if (currentFocus > -1) {
-               
-                if (x) x[currentFocus].click();
-              }
+            if (e.keyCode == 40) {
+
+                currentFocus++;
+
+                addActive(x);
+            } else if (e.keyCode == 38) {
+                currentFocus--;
+
+                addActive(x);
+            } else if (e.keyCode == 13) {
+
+                e.preventDefault();
+                if (currentFocus > -1) {
+
+                    if (x) x[currentFocus].click();
+                }
             }
         });
         function addActive(x) {
-          
-          if (!x) return false;
-          
-          removeActive(x);
-          if (currentFocus >= x.length) currentFocus = 0;
-          if (currentFocus < 0) currentFocus = (x.length - 1);
-          
-          x[currentFocus].classList.add("autocomplete-active");
+
+            if (!x) return false;
+
+            removeActive(x);
+            if (currentFocus >= x.length) currentFocus = 0;
+            if (currentFocus < 0) currentFocus = (x.length - 1);
+
+            x[currentFocus].classList.add("autocomplete-active");
         }
         function removeActive(x) {
-          
-          for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
-          }
+
+            for (var i = 0; i < x.length; i++) {
+                x[i].classList.remove("autocomplete-active");
+            }
         }
         function closeAllLists(elmnt) {
-        
-          var x = document.getElementsByClassName("autocomplete-items");
-          for (var i = 0; i < x.length; i++) {
-            if (elmnt !== x[i] && elmnt !== inp) {
-            x[i].parentNode.removeChild(x[i]);
-          }
+
+            var x = document.getElementsByClassName("autocomplete-items");
+            for (var i = 0; i < x.length; i++) {
+                if (elmnt != x[i] && elmnt != inp) {
+                    x[i].parentNode.removeChild(x[i]);
+                }
+            }
         }
-      }
-      
-      document.addEventListener("click", function (e) {
-          closeAllLists(e.target);
-      });
-      }
-   
 
-    render() {
-     
+        document.addEventListener("click", function (e) {
+            closeAllLists(e.target);
+        });
+    }
 
-        return (
-            <div className="todoListMain">
-                <div className='header'>
-                    <form autocomplete="off" action="/action_page.php" onSubmit={this.addItem} >
-                        <input ref={(a) => this._inputElement = a}
-                            placeholder="Enter ToDo item">
-                        </input>
-                        <button type="submit" >Add ToDo</button>
-                        <button onClick={this.deleteItem}>Clear Completed</button>
-                    <img src="http://cdn.shopify.com/s/files/1/1940/0783/products/Tomato_timer_01_1024x1024.png?v=1523492026" alt="tomato-pic"></img>
-                        <div className="autocomplete" >
+
+render() {
+
+
+    return (
+        <div className="todoListMain">
+            <div className='header'>
+                <form autocomplete="off" action="/action_page.php" onSubmit={this.addItem} >
+                    <input ref={(a) => this._inputElement = a}
+                        placeholder="Enter ToDo item">
+                    </input>
+                    <button type="submit" >Add ToDo</button>
+                    <button onClick={this.deleteItem}>Clear Completed</button>
+                    <div className="pomodoro-wrapper">
+                        <div id="pomodoro-app">
+                            <div id="container">
+                                <div id="timer">
+                                    <div id="time">
+                                        <span id="minutes">25</span>
+                                        <span id="colon">:</span>
+                                        <span id="seconds">00</span>
+                                    </div>
+                                    <div id="filler"></div>
+                                </div>
+
+                                <div id="buttons">
+                                    <button id="work">Work</button>
+                                    <button id="shortBreak">Short Break</button>
+                                    <button id="longBreak">Long Break</button>
+                                    <button id="stop">Stop</button>
+                                </div>
+                            </div>
+                        </div>
+                        <img src="http://cdn.shopify.com/s/files/1/1940/0783/products/Tomato_timer_01_1024x1024.png?v=1523492026" alt="tomato-pic"></img>
+
+
+                    </div>
+                    <div className="autocomplete" >
                         <input id="myInput" type="text" name="myTodo" placeholder="Search Todo">
                         </input >
-                        
-                        </div>
 
-                    </form>
-                </div>
-                <Todo entries={this.state.items}
-                    delete={this.deleteItem}
-                    markCompleted={this.markCompleted} />
+                    </div>
+
+                </form>
             </div>
-        );
-    }
+            <Todo entries={this.state.items}
+                delete={this.deleteItem}
+                markCompleted={this.markCompleted} />
+
+        </div>
+    );
+}
 }
 
 export default ToDoList;
